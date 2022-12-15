87a2e82034a6790adb4def6696db356ef26faff6
schornb <55115049+schornb@users.noreply.github.com>
2022-12-15 15:04:06 -0500

    added practice trial

--- src/experiment.py
+++ src/experiment.py
@@ -31,8 +31,11 @@ expName = 'StartReact Experiment'
 expInfo = {
     'ID': '',
     'Blocks': 5, 
-    'Trials per Block': 15,
-    'Other Notes': ''
+    'Trials per Block': 30,
+    'Trial Start': 0,
+    # ^ pick what trial you want to start at if you need to restart the experiment
+    # check 
+    'Other Notes': '',
 }
 
 dlg = gui.DlgFromDict(dictionary=expInfo, sortKeys=False, title=expName)
@@ -44,6 +47,8 @@ if dlg.OK == False:
 ID = expInfo['ID']
 NUM_BLOCKS = expInfo['Blocks']
 NUM_TRIALS = expInfo['Trials per Block']
+DELAY_MIN = 7 # seconds
+DELAY_MAX = 12 # seconds
 ####
 expInfo['Date'] = data.getDateStr()  # add a simple timestamp
 expInfo['expName'] = expName
@@ -67,7 +72,9 @@ win = win = visual.Window([800,600], fullscr=False, monitor="testMonitor", units
 
 # Instruction
 
-INSTRUCTION_TEXT = "Press any button to start the trial. Prepare your arm to flex and wait for the circle to appear to move"
+INSTRUCTION_TEXT = "Press any button to start the trial. Prepare to raise your arm to the side as fast as you can"
+# Have different instructions per block
+# Prepare to raise your arm to the side as fast as you can
 
 instruction = visual.TextStim(win, 
         text=INSTRUCTION_TEXT
@@ -84,7 +91,7 @@ fixation = visual.ShapeStim(win,
 
 # Stimulus circle
 stimulus = visual.Circle(win, 
-        radius=5,
+        radius=7,
         fillColor = 'red',
         lineColor = 'red',
         autoLog=True
@@ -92,7 +99,7 @@ stimulus = visual.Circle(win,
 
 ####
 # Stimulus Parameters
-STIMULUS_DURATION = 1/10 # seconds
+STIMULUS_DURATION = 1/20 # seconds
 ####
 
 ####
@@ -100,12 +107,12 @@ STIMULUS_DURATION = 1/10 # seconds
 NUM_SOUNDS = 3
 QUIET_DB = 80 # db
 QUIET_HZ = 500 # Hz
-QUIET_TIME = 2/100 # sec
-LOUD_DB = 115 # db
+QUIET_TIME = 1/20 # sec
+LOUD_DB = 120 # db
 LOUD_HZ = 500 # Hz
-LOUD_TIME = 2/100 # sec
-SYNC_DB = 115 # db
-SYNC_HZ = 100 # Hz
+LOUD_TIME = 1/20 # sec
+SYNC_DB = 80 # db
+SYNC_HZ = 500 # Hz
 SYNC_TIME = 100e-3 # sec
 ####
 
@@ -131,23 +138,28 @@ for block in range(NUM_BLOCKS):
 
     # add stimulus of 3 X 100 Hz tones lasting for 100 ms with 200 ms between them
     sync = get_audio(SYNC_DB, SYNC_HZ, SYNC_TIME)
-    sync.autoLog = True;
+    sync.autoLog = True
     now = core.getTime()
     sync.play(when=now, log=True)
     
     sync = get_audio(SYNC_DB, SYNC_HZ, SYNC_TIME)
-    sync.autoLog = TRUE;
+    sync.autoLog = TRUE
     snow = core.getTime()
     sync.play(when=now+200e-3, log=True)
 
     sync = get_audio(SYNC_DB, SYNC_HZ, SYNC_TIME)
-    sync.autoLog = TRUE;
+    sync.autoLog = TRUE
     now = core.getTime()
     sync.play(when=now+200e-3, log=True)
 
     # Set sound
-    sounds = np.tile(np.arange(NUM_SOUNDS), int(NUM_TRIALS/NUM_SOUNDS))
-    np.random.shuffle(sounds)
+
+    # First trial is practice, so order is set
+    if block == 0:
+        sounds = np.arange(NUM_SOUNDS)
+    else:
+        sounds = np.tile(np.arange(NUM_SOUNDS), int(NUM_TRIALS/NUM_SOUNDS))
+        np.random.shuffle(sounds)
 
     # Click to start block
     block_text = visual.TextStim(win, 
@@ -159,11 +171,12 @@ for block in range(NUM_BLOCKS):
     total_time = 0
     current_time = core.getTime()
 
+    # Add practice trial (one of each sound)
+
     for trial in range(NUM_TRIALS):
 
-        
         # Fixation
-        time_up = randint(5, 10) # Picks pause time between 5 and 10 seconds
+        time_up = randint(DELAY_MIN, DELAY_MAX) # Picks pause time between 5 and 10 seconds
         draw_visual(win, fixation, time_up)
 
         # Stimulus
@@ -187,7 +200,7 @@ for block in range(NUM_BLOCKS):
         total_time += (core.getTime() - current_time)
         current_time = core.getTime()
 
-        logFile.write("Block %d, Trial %d, Loadness: %d, Pause: %d, Stimulus Time: %d \n" %(block+1, trial+1, sound_picker, time_up, total_time))
+        logFile.write("Block %d, Trial %d, Loadness: %d, Pause: %d, Trial Time: %d \n" %(block+1, trial+1, sound_picker, time_up, total_time))
         print("Block %d, Trial %d, Loadness: %d, Pause: %d \n" %(block+1, trial+1, sound_picker, time_up))
         logging.flush()
 
